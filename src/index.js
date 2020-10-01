c482be27215cbd4c4d47021f8166fb2e60e6f97c
Nickita Solovei <nickaya112@gmail.com>
2020-10-01 16:45:10 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (arguments.length === 0) {return 0};
+  return array.reduce((prev, item) => Math.min(prev, item), 0);
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (arguments.length === 0) {return 0};
+  return array.reduce((prev, item) => Math.max(prev, item), 0);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (arguments.length === 0 || array.length === 0) {return 0};
+  return array.reduce((prev, item) => prev + item) / array.length;
 }
